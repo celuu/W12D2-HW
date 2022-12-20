7197ba4486ca26baaccc31371b737b11eb2cdca0
Nishant Racherla <nishant.racherla@gmail.com>
2022-12-20 11:13:01 -0800

    added explanation for wrap_parameters

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,9 @@
 class Api::UsersController < ApplicationController
+  # the frontend sends in data without nesting it under a "user" key
+  # to make our backend take in this data without breaking the user params
+  # this line wraps all the incoming data under the name of the controller
+  # User.attribute_names returns a list of all the columns in the db
+  # but since "password" isnt a field in our db, we have to add it on
   wrap_parameters include: User.attribute_names + ['password']
 
   def create
